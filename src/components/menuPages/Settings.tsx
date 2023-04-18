107ff3177644ca635f0de339d84624f0f874ce43
LandonDoyle7599 <landondoyle7599@gmail.com>
2023-04-17 18:42:45 -0600

    ready for approval

--- src/components/menuPages/Settings.tsx
+++ src/components/menuPages/Settings.tsx
@@ -1,10 +1,7 @@
 import {
   Button,
-  Dialog,
-  Divider,
   FormControl,
   InputLabel,
-  ListItem,
   MenuItem,
   Select,
   Stack,
@@ -17,17 +14,16 @@ import ScreenHeader from "./ScreenHeader";
 type controlKey = "left" | "right" | "shoot";
 
 const defaultScheme = {
-  left: "Left Arrow",
-  right: "Right Arrow",
+  left: "ArrowLeft",
+  right: "ArrowRight",
   shoot: "Space",
 };
 
 export const Settings: FC<ScreenProps> = ({ onBack }) => {
   const [controlScheme, setControlScheme] = useState(defaultScheme);
-  const [controlToChange, setControlToChange] = useState<controlKey>(
-    "left"
-  );
-  const [keyToChange, setKeyToChange] = useState<string>("Change Key");
+  const [controlToChange, setControlToChange] = useState<controlKey>("left");
+  const [keyToChange, setKeyToChange] = useState<string>("Click to select new key");
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     const controlScheme = localStorage.getItem("controlScheme");
@@ -40,18 +36,41 @@ export const Settings: FC<ScreenProps> = ({ onBack }) => {
   }, []);
 
   function setControl(key: controlKey, value: string) {
-    const controlScheme = localStorage.getItem("controlScheme");
-    if (controlScheme) {
-      const parsedControlScheme = JSON.parse(controlScheme);
+    const existingControlScheme = localStorage.getItem("controlScheme");
+    if (existingControlScheme) {
+      const parsedControlScheme = JSON.parse(existingControlScheme);
       parsedControlScheme[key] = value;
       localStorage.setItem(
         "controlScheme",
         JSON.stringify(parsedControlScheme)
       );
+      setControlScheme(parsedControlScheme);
     } else {
-      defaultScheme[key] = value;
-      localStorage.setItem("controlScheme", JSON.stringify(defaultScheme));
+      const newScheme = {...defaultScheme};
+      newScheme[key] = value;
+      localStorage.setItem("controlScheme", JSON.stringify(newScheme));
+      setControlScheme(newScheme);
     }
+  }
+
+  function remapButtonClick() {
+    setError(false);
+    setKeyToChange("Press new key for control");
+    const keyPress = (e: KeyboardEvent) => {
+      e.preventDefault();
+      if(e.key === "Escape" || Object.values(controlScheme).includes(e.key) || (Object.values(controlScheme).includes("Space") && e.key === " ")){
+        setError(true);
+        setKeyToChange("Invalid Key");
+        return;
+      }
+      if (e.key === " ") {
+        setKeyToChange("Space");
+      }else{
+        setKeyToChange(e.key);
+      }
+      window.removeEventListener("keydown", keyPress);
+    }
+    window.addEventListener("keydown", keyPress);
   }
 
   return (
@@ -67,35 +86,39 @@ export const Settings: FC<ScreenProps> = ({ onBack }) => {
         Shoot - {controlScheme.shoot}
       </Typography>
       <FormControl fullWidth>
-        <InputLabel>Control</InputLabel>
+        <InputLabel>Control to Change</InputLabel>
         <Select
           value={controlToChange}
           label="Control"
           onChange={(e) => setControlToChange(e.target.value as controlKey)}
         >
-          <MenuItem value={"left"}>Left</MenuItem>
-          <MenuItem value={"right"}>Right</MenuItem>
+          <MenuItem value={"left"}>Move Left</MenuItem>
+          <MenuItem value={"right"}>Move Right</MenuItem>
           <MenuItem value={"shoot"}>Shoot</MenuItem>
         </Select>
       </FormControl>
-        <Button
-            onClick={() => {
-                setKeyToChange("Press a key");
-                const keyPress = (e: KeyboardEvent) => {
-                    setKeyToChange(e.key);
-                    window.removeEventListener("keydown", keyPress);
-                };
-                window.addEventListener("keydown", keyPress);
-            }}
-        >
-            {keyToChange}
-        </Button>
-        <Button onClick={() => {
-            setControl(controlToChange, keyToChange)
-            setKeyToChange("Change Key");
-        }}> 
-            Remap Selected Control
-        </Button>
+      <Button
+        onClick={() => {
+          remapButtonClick();
+        }}
+      >
+        {keyToChange}
+      </Button>
+      <Button
+        disabled={keyToChange === "Click to select new key" || keyToChange === "Press new key for control" || keyToChange === "Escape" || error}
+        onClick={() => {
+          setControl(controlToChange, keyToChange);
+          setKeyToChange("Click to select new key");
+        }}
+      >
+        Remap Selected Control
+      </Button>
+      <Button onClick={() => {
+        localStorage.setItem("controlScheme", JSON.stringify(defaultScheme));
+        setControlScheme(defaultScheme);
+      }}>
+        Reset Controls to Default
+      </Button>
     </Stack>
   );
 };
--- src/game/helpers/utils.ts
+++ src/game/helpers/utils.ts
@@ -1,16 +1,28 @@
 import { Keys } from "./types";
 
 export function addEventListeners(keys: Keys) {
+  const defaultControlScheme = {
+    left: "ArrowLeft",
+    right: "ArrowRight",
+    shoot: "Space",
+  };
+  const controlScheme = localStorage.getItem("controlScheme");
+  let parsedControlScheme = defaultControlScheme;
+  if (controlScheme) {
+    parsedControlScheme = JSON.parse(controlScheme);
+  } else {
+    localStorage.setItem("controlScheme", JSON.stringify(defaultControlScheme));
+  }
   window.addEventListener("keydown", ({ key }) => {
-    if (key === "ArrowRight") keys.right = true;
-    if (key === "ArrowLeft") keys.left = true;
-    if (key === " ") keys.shoot = true;
+    if (key === parsedControlScheme.right || (parsedControlScheme.right === "Space" && key === " ")) keys.right = true;
+    if (key === parsedControlScheme.left || (parsedControlScheme.left === "Space" && key === " ")) keys.left = true;
+    if (key === parsedControlScheme.shoot || (parsedControlScheme.shoot === "Space" && key === " ")) keys.shoot = true;
     if (key === "Escape") keys.escape = true;
   });
 
   window.addEventListener("keyup", ({ key }) => {
-    if (key === "ArrowRight") keys.right = false;
-    if (key === "ArrowLeft") keys.left = false;
+    if (key === parsedControlScheme.right || (parsedControlScheme.right === "Space" && key === " ")) keys.right = false;
+    if (key === parsedControlScheme.left || (parsedControlScheme.left === "Space" && key === " ")) keys.left = false;
   });
 }
 
